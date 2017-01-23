51a86194204b7bad4a5ac2cf20fb94fe7f14f5fd
gbarth1 <grace.m.barth@gmail.com>
2017-01-23 14:30:49 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        String color = in.next();
+
+        System.out.println("PLease enter in a")
     }
 }
